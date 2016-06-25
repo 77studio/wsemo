d107a2996668fd4e0baaf8a61ce7e111b6c12508
Yan Jian <yanjian_cn@163.com>
2016-06-25 17:04:30 +0800

    Message route via long job id.

---
+++ README.md
@@ -0,0 +1,23 @@
+For BDMP usage.
+
+# Test steps
+* 1. launch
+``` shell
+mvn clean spring-boot:run
+```
+
+* 2. browser 1: subscribe as job id 1
+open http://localhost:8080/ in browser, and input '1' then click connect button.
+
+* 3. browser 2: subscribe as job id 2
+open http://localhost:8080/ in browser, and input '2' then click connect button.
+
+* 4. send message to [job id 1]'s subscribes
+open http://localhost:8080/testSend?id=1&msg=longjob1 in browser
+browser1 will receive this message.
+
+* 5. send message to [job id 2]'s subscribes
+open http://localhost:8080/testSend?id=2&msg=longjob2 in browser
+browser2 will receive this message.
+
+*while step 4 and 5 can be act more times.*
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,18 +1,39 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
 
+    @Autowired
+    private SimpMessagingTemplate template;
 
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(3000); // simulated delay
-        return new Greeting("Hello, " + message.getName() + "!");
+    /**
+     * 测试用发送消息
+     * @param msg 消息内容
+     * @param id 长任务ID
+     * @return
+     */
+    @RequestMapping("/testSend")
+    @ResponseBody
+    public Greeting send(@RequestParam String msg, @RequestParam String id) {
+        Greeting greeting = new Greeting("Hello, " + msg + "!");
+        // 推送消息，id：LongJob的ID     greeting：消息内容对象
+        template.convertAndSendToUser(id, "/message", greeting);
+        return greeting;
     }
 
+    @MessageMapping("/message")
+    @SendToUser("/message")
+    public Greeting userMessage(HelloMessage message) throws Exception {
+        System.err.println("Job: " + message.getName());
+        return new Greeting("Job: " + message.getName());
+    }
 }
